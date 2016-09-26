bb7a1c14a6e35419b526610ec60e61143bbabf91
Duy Do <ddo@csu.fullerton.edu>
2016-09-26 15:19:16 -0700

    Added another textbox and button for comments from anoter user

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,27 +1,54 @@
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
 var main = function () {
     "use strict";
 
-    var addCommentFromInputBox = function () {
-        var $new_comment;
+    var addCommentFromInputBox = function (user) {
+        var $newComment;
 
-        if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input input").val("");
+      	if(user == 1)
+        {         
+        	if ($("#user1 input").val() !== "") {
+                $newComment = $("<p>").addClass("userA").text
+                          ($("#user1 input").val());
+                $newComment.hide();
+                $(".comments").append($newComment);
+                $newComment.fadeIn();
+                $("#user1 input").val("");
+        	}
+        }
+        else if (user == 2)
+        {
+    		if ($("#user2 input").val() !== "") {
+                $newComment = $("<p>").addClass("userB").text
+                          ($("#user2 input").val());
+                $newComment.hide();
+                $(".comments").append($newComment);
+                $newComment.fadeIn();
+                $("#user2 input").val("");
+        	}
         }
     };
 
-    $(".comment-input button").on("click", function (event) {
-        addCommentFromInputBox();
+    $("#user1 button").on("click", function (event) {
+        addCommentFromInputBox(1);
     });
 
-    $(".comment-input input").on("keypress", function (event) {
+    $("#user2 button").on("click", function (event) {
+        addCommentFromInputBox(2);
+    });
+
+    $("#user1 input").on("keypress", function (event) {
         if (event.keyCode === 13) {
-            addCommentFromInputBox();
+            addCommentFromInputBox(1);
         }
     });
+
+    $("#user2 input").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+            addCommentFromInputBox(2);
+        }
+    });
+
 };
 
 $(document).ready(main);

0c3c71d3b490d3e65db7629bde26b68670745086
Duy Do <ddo@csu.fullerton.edu>
2016-09-27 18:30:23 -0700

    modified chat app

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,49 +1,49 @@
 /* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
 var main = function () {
-    "use strict";
+    'use strict';
 
     var addCommentFromInputBox = function (user) {
         var $newComment;
 
-      	if(user == 1)
+      	if(user === 1)
         {         
-        	if ($("#user1 input").val() !== "") {
-                $newComment = $("<p>").addClass("userA").text
-                          ($("#user1 input").val());
+        	if ($('#user1 input').val() !== '') {
+                $newComment = $('<p>').addClass('userA').text
+                          ($('#user1 input').val());
                 $newComment.hide();
-                $(".comments").append($newComment);
+                $('.comments').append($newComment);
                 $newComment.fadeIn();
-                $("#user1 input").val("");
+                $('#user1 input').val('');
         	}
         }
-        else if (user == 2)
+        else if (user === 2)
         {
-    		if ($("#user2 input").val() !== "") {
-                $newComment = $("<p>").addClass("userB").text
-                          ($("#user2 input").val());
+    		if ($('#user2 input').val() !== '') {
+                $newComment = $('<p>').addClass('userB').text
+                          ($('#user2 input').val());
                 $newComment.hide();
-                $(".comments").append($newComment);
+                $('.comments').append($newComment);
                 $newComment.fadeIn();
-                $("#user2 input").val("");
+                $('#user2 input').val('');
         	}
         }
     };
 
-    $("#user1 button").on("click", function (event) {
+    $('#user1 button').on('click', function (event) {
         addCommentFromInputBox(1);
     });
 
-    $("#user2 button").on("click", function (event) {
+    $('#user2 button').on('click', function (event) {
         addCommentFromInputBox(2);
     });
 
-    $("#user1 input").on("keypress", function (event) {
+    $('#user1 input').on('keypress', function (event) {
         if (event.keyCode === 13) {
             addCommentFromInputBox(1);
         }
     });
 
-    $("#user2 input").on("keypress", function (event) {
+    $('#user2 input').on('keypress', function (event) {
         if (event.keyCode === 13) {
             addCommentFromInputBox(2);
         }
